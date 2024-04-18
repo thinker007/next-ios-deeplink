ce6bdd4bbda86c84d035742497ca03c65149e9cc
thinker007 <thinkerisme@gmail.com>
2024-04-18 17:37:11 +0800

    Update ios-deeplink.js

--- pages/api/ios-deeplink.js
+++ pages/api/ios-deeplink.js
@@ -2,6 +2,8 @@
 
 export default (req, res) => {
   const { pair } = req.query || {};
+  //url 转义处理 支持中文和特殊字符
+  pair = encodeURIComponent(pair)
 
   res.writeHead(301, {
     location: `imessage://chat2gpt%40icloud.com&body=${pair}`,
