5cfef2738f9d91b053709a9bf6a47ec5ef271faa
thinker007 <thinkerisme@gmail.com>
2024-12-11 10:29:12 +0800

    Update ios-deeplink.js

--- pages/api/ios-deeplink.js
+++ pages/api/ios-deeplink.js
@@ -1,10 +1,10 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default (req, res) => {
-  const { pair, email = 'chat%40chat2gpt4.com' } = req.query || {};
+  const { pair = '你好', email = 'chat@chat2gpt4.com' } = req.query || {};
   //url 转义处理 支持中文和特殊字符
-  const encodedpair = encodeURIComponent(pair)
-  const encodedEmail = encodeURIComponent(email.includes('%40') ? email : email.replace('@', '%40'));
+  const encodedpair = encodeURIComponent(pair);
+  const encodedEmail = encodeURIComponent(email);
 
   res.writeHead(301, {
     location: `imessage://${encodedEmail}&body=${encodedpair}`,
