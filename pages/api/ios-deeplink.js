66d7ce1051e4c820aad2d01897fae7f437f57716
thinker007 <thinkerisme@gmail.com>
2024-12-11 10:20:06 +0800

    Update ios-deeplink.js
    支持在 url 中指定 email 并且 默认 id 改为：chat@chat2gpt4.com

--- pages/api/ios-deeplink.js
+++ pages/api/ios-deeplink.js
@@ -1,12 +1,13 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default (req, res) => {
-  const { pair } = req.query || {};
+  const { pair, email = 'chat%40chat2gpt4.com' } = req.query || {};
   //url 转义处理 支持中文和特殊字符
   const encodedpair = encodeURIComponent(pair)
+  const encodedEmail = encodeURIComponent(email.includes('%40') ? email : email.replace('@', '%40'));
 
   res.writeHead(301, {
-    location: `imessage://chat2gpt%40icloud.com&body=${encodedpair}`,
+    location: `imessage://${encodedEmail}&body=${encodedpair}`,
   });
   res.end();
 };
